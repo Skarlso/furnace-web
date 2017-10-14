473783f9816711e849463ffde3bbf0bd88de28a2
Gergely Brautigam <skarlso777@gmail.com>
2017-10-14 21:37:15 +0200

    Fixed parallax, maybe.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $('.materialboxed').materialbox();
-});
-$(document).ready(function(){
-  $('.materialboxed').materialbox();
-});
-$(document).ready(function(){
   $('.parallax').parallax();
+});
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
 });
